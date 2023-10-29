3508a5d08d0839f2479fb48a6d15fa1da689d494
1F616EMO <root@1f616emo.xyz>
2023-10-29 16:51:40 +0800

    2.0

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# worship-lyrics
+# Create PowerPoint for worship lyrics
+
+## File Format
+
+### Starting Line
+
+Every file must start with this line:
+
+```text
+!VER 2
+```
+
+This is the version indicator to avoid incompatible versions of this program in processing the lyrics file.
+
+### Background image
+
+The background image of furture slides can be defined with:
+
+```text
+!BKG <path>
+```
+
+Where `<path>` is the path to the image, either absolute or relative to the lyrics file.
+
+### Fonts
+
+Both lyrics and footers can be customized. The following can be customized:
+
+* `FONT`: The name of the font to be used. It is recommended to include a font that is known to exist on both your computer and the one presenting the PowerPoint.
+* `SIZE`: Font size in Pt. This should be a valid floating point number.
+* `COLOR`: 8-bit hexadecimal RGB color. For example, `000000` represents pure black.
+
+Commands in the format of `!<scope>-<type>`, where `<scope>` can be either `LYRICS` or `FOOTER`, define the format of texts in that scope occurring after this statement. Note that the occurring time of the footer is the time a new slide is created, not the time the footer is defined.
+
+Commands in the format of `!<type>` imply the command of both scopes.
+
+For example, the following snippet defines the font for both lyrics and footers, then defines different colors and sizes for each:
+
+```text
+!FONT Arial
+!LYRICS-COLOR FFFFFF
+!FOOTER-COLOR 000000
+!LYRICS-SIZE 60
+!FOOTER-SIZE 24
+```
+
+### Lyrics Text
+
+A block of lyric texts is separated from other lyric texts by two spaces, or in other words, there is one empty line between two lyric text blocks. For example, the following snippet represents two lyric text blocks:
+
+```text
+My table thou hast furnished
+in presence of my foes:
+
+my head thou dost with oil anoint,
+and my cup overflows.
+```
+
+Each lyric text blocks will be put into individual slides.
+
+### Sections
+
+Sections are wrapped with `!SECTION-START <section-name>` and `SECTION-END`. For example, the following snippet creates a section called "`Shep-C`" with two lyric text blocks:
+
+```text
+!SECTION-START Shep-C
+Goodness and mercy all my life
+shall surely follow me.
+
+And in God's house for evermore
+my dwelling place shall be. 
+!SECTION-END
+```
+
+A section does not do anything on its own. They can be used via `!SECTION <section-name>`. For example, the following snippet repeats the above section twice:
+
+```text
+!SECTION Shep-C
+!SECTION Shep-C
+```
+
+Note that a section must be defined before their usage, and cannot be defined more than once using the same name.
+
+### Indication of empty slides
+
+An indication of an empty slide is declared as:
+
+```text
+!EMPTY
+```
+
+This creates a footer-only slide without any lyrics.
+
+## Examples
+
+Examples of this program can be found in `examples/`. Unless otherwise specified, all lyrics and images inside are in the Public Domain.
--- worship_lyrics.py
+++ worship_lyrics.py
@@ -1,47 +1,45 @@
 from pptx import Presentation
 from pptx.util import Inches, Pt
-from pptx.slide import SlideLayout
 from pptx.enum.shapes import MSO_SHAPE
 from pptx.enum.text import PP_ALIGN, MSO_ANCHOR
 from pptx.dml.color import RGBColor
+from warnings import warn
+import os.path as path
 
 
-def read_data(filetext: str) -> tuple:
-    filetext = filetext.replace("\r\n", "\n")
-    data = tuple(tuple(s.strip().split("\n")) for s in filetext.split("\n\n"))
-    return data
+class ParseError(RuntimeError):
+    def __init__(self, line, message):
+        real_msg = "On line {}: {}".format(line, message)
+        super().__init__(real_msg)
 
 
-def validate_data(data: tuple):
-    if len(data[0]) != 4:
-        raise TypeError(
-            "First data (name, copyright, image_path, text_color) must be 4")
-    return True
+class ParseWarning(Warning):
+    def __init__(self, message):
+        real_msg = "On line {}: {}".format(message[0], message[1])
+        super().__init__(real_msg)
 
 
 def initialize_pptx() -> Presentation:
-    pre = Presentation()
-    pre.slide_height = Inches(9)
-    pre.slide_width = Inches(16)
+    prs = Presentation()
+    prs.slide_height = Inches(9)
+    prs.slide_width = Inches(16)
 
-    return pre
-
-
-BLANK_SLIDE_LAYOUT = 6  # Blank
-FONT = "微軟正黑體"
+    return prs
 
 
-def add_slide(prs, lyrics, header_data):
-    slide = prs.slides.add_slide(prs.slide_layouts[BLANK_SLIDE_LAYOUT])
+def create_slide(prs: Presentation, curr_line, curr_lyrics, vars):
+    slide = prs.slides.add_slide(prs.slide_layouts[6])
     shapes = slide.shapes
-    color = RGBColor.from_string(header_data[3])
-    # Add background image
-    shapes.add_picture(
-        header_data[2], 0, 0,
-        height=prs.slide_height, width=prs.slide_width
-    )
-    # Add lyrics text
-    if lyrics[0] != "#EMPTY!":  # Use "#EMPTY!" to skip a slide
+    # background image
+    if "BKG" in vars and vars["BKG"] != "":
+        shapes.add_picture(
+            vars["BKG"], 0, 0,
+            height=prs.slide_height, width=prs.slide_width
+        )
+    else:
+        warn((curr_line, "No background image while creating slides"), ParseWarning)
+    # lyrics text
+    if curr_lyrics != "":
         text_shape = shapes.add_shape(
             MSO_SHAPE.RECTANGLE, Inches(0.5), Inches(
                 1.5), Inches(15), Inches(6)
@@ -52,40 +50,220 @@ def add_slide(prs, lyrics, header_data):
         text_frame.clear()
         text_frame.vertical_anchor = MSO_ANCHOR.TOP
         text_paragraph = text_frame.paragraphs[0]
-        text_paragraph.text = "\n".join(lyrics)
+        text_paragraph.text = curr_lyrics
         text_paragraph.alignment = PP_ALIGN.CENTER
-        text_paragraph.font.size = Pt(60)
-        text_paragraph.font.name = FONT
+        # lyrics font size
+        if "LYRICS-SIZE" in vars and vars["LYRICS-SIZE"] != "":
+            try:
+                font_size = float(vars["LYRICS-SIZE"])
+            except ValueError as e:
+                raise ParseError(curr_line,
+                                 "Invalid lyrics size {}".format(vars["LYRICS-SIZE"])) from e
+            text_paragraph.font.size = Pt(font_size)
+        else:
+            warn((curr_line, "No lyrics font size while creating slides"), ParseWarning)
+        # lyrics font name
+        if "LYRICS-FONT" in vars and vars["LYRICS-FONT"] != "":
+            font_name = vars["LYRICS-FONT"]
+            text_paragraph.font.name = font_name
+        else:
+            warn((curr_line, "No lyrics font name while creating slides"), ParseWarning)
+        # lyrics font color
+        if "LYRICS-COLOR" in vars and vars["LYRICS-COLOR"] != "":
+            try:
+                font_color = RGBColor.from_string(vars["LYRICS-COLOR"])
+            except ValueError as e:
+                raise ParseError(curr_line,
+                                 "Invalid lyrics color {}".format(vars["LYRICS-COLOR"])) from e
+            text_paragraph.font.color.rgb = font_color
         text_paragraph.font.bold = True
-        text_paragraph.font.color.rgb = color
-    # Add copyright text
-    cpr_shape = shapes.add_shape(
-        MSO_SHAPE.RECTANGLE, Inches(0.5), Inches(6.5), Inches(10), Inches(2)
-    )
-    cpr_shape.fill.background()
-    cpr_shape.line.fill.background()
-    cpr_frame = cpr_shape.text_frame
-    cpr_frame.clear()
-    cpr_frame.vertical_anchor = MSO_ANCHOR.BOTTOM
-    cpr_paragraph = cpr_frame.paragraphs[0]
-    cpr_paragraph.text = "\n".join(header_data[:2])
-    cpr_paragraph.alignment = PP_ALIGN.LEFT
-    cpr_paragraph.font.size = Pt(24)
-    cpr_paragraph.font.name = FONT
-    cpr_paragraph.font.bold = True
-    cpr_paragraph.font.color.rgb = color
+    # footer
+    if "FOOTER" in vars and vars["FOOTER"] != "":
+        text_shape = shapes.add_shape(
+            MSO_SHAPE.RECTANGLE, Inches(0.5), Inches(
+                6.5), Inches(10), Inches(2)
+        )
+        text_shape.fill.background()
+        text_shape.line.fill.background()
+        text_frame = text_shape.text_frame
+        text_frame.clear()
+        text_frame.vertical_anchor = MSO_ANCHOR.BOTTOM
+        text_paragraph = text_frame.paragraphs[0]
+        text_paragraph.text = vars["FOOTER"]
+        text_paragraph.alignment = PP_ALIGN.LEFT
+        # footer font size
+        if "FOOTER-SIZE" in vars and vars["FOOTER-SIZE"] != "":
+            try:
+                font_size = float(vars["FOOTER-SIZE"])
+            except ValueError as e:
+                raise ParseError(curr_line,
+                                 "Invalid footer size {}".format(vars["FOOTER-SIZE"])) from e
+            text_paragraph.font.size = Pt(font_size)
+        else:
+            warn((curr_line, "No footer font size while creating slides"), ParseWarning)
+        # footer font name
+        if "FOOTER-FONT" in vars and vars["FOOTER-FONT"] != "":
+            font_name = vars["FOOTER-FONT"]
+            text_paragraph.font.name = font_name
+        else:
+            warn((curr_line, "No footer font name while creating slides"), ParseWarning)
+        # footer font color
+        if "FOOTER-COLOR" in vars and vars["FOOTER-COLOR"] != "":
+            try:
+                font_color = RGBColor.from_string(vars["FOOTER-COLOR"])
+            except ValueError as e:
+                raise ParseError(curr_line,
+                                 "Invalid footer color {}".format(vars["FOOTER-COLOR"])) from e
+            text_paragraph.font.color.rgb = font_color
+        text_paragraph.font.bold = True
 
 
-def workflow(filename, savefilename):
-    with open(filename, mode='rb') as f:
-        b = f.read()
-        filetext = str(b, encoding='utf-8')
-    data = read_data(filetext)
-    validate_data(data)
+def ensure_has_param(curr_line, cmdline, curr_state):
+    if cmdline[1] == "":
+        raise ParseError(curr_line,
+                         "Missing argument for command {} in state {}".format(cmdline[0], curr_state))
+
+
+def interpepter(filename, savefilename):
+    vars = {}
+    curr_lyrics = ""
+    curr_state = "read"
+    curr_line = 1
     prs = initialize_pptx()
-    for lyrics in data[1:]:
-        add_slide(prs, lyrics, data[0])
-    prs.save(savefilename)
+    file_dir = path.dirname(filename)
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if curr_line == 1:
+                if line != "!VER 2":
+                    raise ParseError(
+                        1, "First line of lyrics file must be \"!VER 2\", got \"{}\" instead".format(line))
+            elif len(line) > 0 and line[0] == "#":
+                continue
+            else:
+                match curr_state:
+                    case "read":
+                        if line == "":
+                            if curr_lyrics != "":
+                                create_slide(prs, curr_line, curr_lyrics, vars)
+                                curr_lyrics = ""
+                        elif line[0] == "!":
+                            cmdline = line[1:].split(" ", 1)
+                            if len(cmdline) == 1:
+                                cmdline.append("")
+                            cmdline[1] = cmdline[1].strip()
+                            match cmdline[0]:
+                                case "BKG":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["BKG"] = path.join(
+                                        file_dir, cmdline[1])
+                                case "FONT":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-FONT"] = cmdline[1]
+                                    vars["FOOTER-FONT"] = cmdline[1]
+                                case "LYRICS-FONT":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-FONT"] = cmdline[1]
+                                case "FOOTER-FONT":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["FOOTER-FONT"] = cmdline[1]
+                                case "COLOR":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-COLOR"] = cmdline[1]
+                                    vars["FOOTER-COLOR"] = cmdline[1]
+                                case "LYRICS-COLOR":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-COLOR"] = cmdline[1]
+                                case "FOOTER-COLOR":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["FOOTER-COLOR"] = cmdline[1]
+                                case "SIZE":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-SIZE"] = cmdline[1]
+                                    vars["FOOTER-SIZE"] = cmdline[1]
+                                case "LYRICS-SIZE":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["LYRICS-SIZE"] = cmdline[1]
+                                case "FOOTER-SIZE":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    vars["FOOTER-SIZE"] = cmdline[1]
+                                case "EMPTY":
+                                    if curr_lyrics != "":
+                                        raise ParseError(curr_line,
+                                                         "Attempt to execute !EMPTY with lyrics")
+                                    create_slide(prs, curr_line, "", vars)
+                                case "SECTION":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    if curr_lyrics != "":
+                                        raise ParseError(curr_line,
+                                                         "Attempt to execute !SECTION with lyrics")
+                                    if ("SECTION-" + cmdline[1]) not in vars:
+                                        raise ParseError(curr_line,
+                                                         "Attempt to refer to invalid section {}".format(cmdline[0]))
+                                    section_lyrics = tuple(
+                                        s.strip() for s in vars["SECTION-" + cmdline[1]].split("\n\n"))
+                                    for s in section_lyrics:
+                                        create_slide(
+                                            prs, curr_line, s, vars)
+                                case "FOOTER-START":
+                                    if curr_lyrics != "":
+                                        raise ParseError(curr_line,
+                                                         "Attempt to execute !FOOTER-START with lyrics")
+                                    curr_state = "cmd-FOOTER"
+                                case "SECTION-START":
+                                    ensure_has_param(
+                                        curr_line, cmdline, curr_state)
+                                    if curr_lyrics != "":
+                                        raise ParseError(curr_line,
+                                                         "Attempt to execute !SECTION-START with lyrics")
+                                    if ("SECTION-" + cmdline[1]) in vars:
+                                        raise ParseError(curr_line,
+                                                         "Attempt to redefine section {}".format(cmdline[0]))
+                                    vars["CURR-SECTION"] = cmdline[1]
+                                    curr_state = "cmd-SECTION"
+                                case _:
+                                    raise ParseError(curr_line,
+                                                     "Invalid command {} in state {}".format(cmdline[0], curr_state))
+                        else:
+                            curr_lyrics += line + "\n"
+                    case "cmd-FOOTER":
+                        if len(line) > 0 and line[0] == "!":
+                            if line == "!FOOTER-END":
+                                vars["FOOTER"] = curr_lyrics
+                                curr_lyrics = ""
+                                curr_state = "read"
+                            else:
+                                raise ParseError(
+                                    curr_line, "Attempt to invoke command in state {}".format(curr_state))
+                        else:
+                            curr_lyrics += line + "\n"
+                    case "cmd-SECTION":
+                        if len(line) > 0 and line[0] == "!":
+                            if line == "!SECTION-END":
+                                vars["SECTION-" + vars["CURR-SECTION"]
+                                     ] = curr_lyrics
+                                del vars["CURR-SECTION"]
+                                curr_lyrics = ""
+                                curr_state = "read"
+                            else:
+                                raise ParseError(
+                                    curr_line, "Attempt to invoke command in state {}".format(curr_state))
+                        else:
+                            curr_lyrics += line + "\n"
+            curr_line += 1
+        # end for line in f
+        prs.save(savefilename)
 
 
 if __name__ == "__main__":
@@ -95,7 +273,7 @@ if __name__ == "__main__":
     parser.add_argument('savefilename')
     args = parser.parse_args()
 
-    workflow(args.filename, args.savefilename)
+    interpepter(args.filename, args.savefilename)
 
     from sys import exit
     exit(0)
